d4b1be461a13f6e23660b56f534cfeb0d453394b
Rajkamal Thillai Manoharan <rajkamal.thillaimanoharan@hotmail.com>
2020-06-01 16:37:35 -0500

    Fixed style and font sizes for post detail page

--- src/content/products/windows-and-doors/windows-and-doors.md
+++ src/content/products/windows-and-doors/windows-and-doors.md
@@ -6,3 +6,5 @@ keywords: "Windows and Doors"
 featured: "true"
 description: "Recognized as the leading brand for global housing and building material industry in Japan, TOSTEM provides a comprehensive range of solutions to ensure comfortable homes for everybody through advanced technologies. We do so by drawing on over a half-century of experience and expertise in developing innovative technology that anticipates not only the needs for all here and now, but also the needs for home living tomorrow."
 ---
+
+Recognized as the leading brand for global housing and building material industry in Japan, TOSTEM provides a comprehensive range of solutions to ensure comfortable homes for everybody through advanced technologies. We do so by drawing on over a half-century of experience and expertise in developing innovative technology that anticipates not only the needs for all here and now, but also the needs for home living tomorrow.
--- src/templates/product.js
+++ src/templates/product.js
@@ -2,6 +2,7 @@ import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 const styles = (theme) => ({
     root: {
@@ -15,6 +16,19 @@ const styles = (theme) => ({
         minWidth: '100%',
         maxHeight: '25vh',
         objectFit: 'cover'
+    },
+    postHeaderContent:{
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    typographyHeader: {
+      color: 'rgba(69, 72, 77, 1)',
+      fontSize: '2rem',
+      alignSelf: 'center',
+    },
+    postContent: {
+      color: 'rgba(69, 72, 77, 1)',
+      fontSize: '1.2rem',
     }
 });
 
@@ -49,15 +63,17 @@ const Product = (props) => {
             <div className={classes.root}>
                 <div className={classes.postHeader}>
                     {/* <p><i>Created on {props.data.markdownRemark.frontmatter.date}</i></p> <br></br> */}
-                    <div>
+                    <div className={classes.postHeaderMedia}>
                         <img 
                             src={props.data.markdownRemark.frontmatter.image.publicURL} 
                             alt={props.data.markdownRemark.frontmatter.title}
                             className={classes.postHeaderImg}
                         />
                     </div>
-                    <div>
-                        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+                    <div className={classes.postHeaderContent}>
+                        <Typography gutterBottom variant="overline" className={classes.typographyHeader}>
+                            {props.data.markdownRemark.frontmatter.title}
+                        </Typography>
                     </div>
                 </div>
                 <div className={classes.postContent} dangerouslySetInnerHTML={{__html : props.data.markdownRemark.html}}>
