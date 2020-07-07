56f0340ba81e0b8a5f820737c060daa9463de286
Rajkamal Thillai Manoharan <rajkamal.thillaimanoharan@hotmail.com>
2020-07-06 23:01:06 -0500

    Update site config for description to improve seo

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Dawnmax",
   shortName: "DM",
   description:
-    "We deliver excellent building customization solutions to our customers to achieve what they envisioned",
+    "We deliver excellent building customization solutions to our customers to achieve what they envisioned | We are official partner for brands such as Lixil Tostem, Imperio Railing Systems and Enox | We deal Doors and Windows, Doors Chennai, Windows Chennai, Railings Solutions Chennai, Handrails Chennai, Lixil Chennai, Tostem Chennai, Enox Chennai, Imperio Chennai, Hardware Chennai",
   url: "https://dawnmax.in", // Domain of your site without prefix!
   prefix: "/",
   author: "Being Artisan",
