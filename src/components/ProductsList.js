baddcbb265bf98e9b4cebc3d74846c0d0494228f
Rajkamal Thillai Manoharan <rajkamal.thillaimanoharan@hotmail.com>
2020-07-07 13:59:02 -0500

    Product list page restructured

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,7 +3,6 @@ import { withStyles} from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import CardX from './CardX';
 import Heading from './Heading';
-import { graphql, StaticQuery } from "gatsby";
 
 const styles = (theme) => ({
     root: {
@@ -18,104 +17,15 @@ const styles = (theme) => ({
       }
 });
 
-
-const query = graphql`
-    query {
-        allMarkdownRemark(sort: {fields: frontmatter___sequence}, filter: {fileAbsolutePath: {regex: "/src/content/products/"}, frontmatter: {featured: {}, productType1: {}, productType2: {}}}) {
-            edges {
-              node {
-                frontmatter {
-                  title
-                  description
-                  date(formatString: "MM/DD/yyyy")
-                  image {
-                    publicURL
-                    childImageSharp {
-                        fluid {
-                          src
-                        }
-                      }
-                  }
-                  link
-                  featured
-                  productType1
-                  productType2
-                }
-                fileAbsolutePath
-                fields {
-                  slug
-                }
-              }
-            }
-          }
-        }  
-`;
-
-const getQueryParams = () => {
-    let params = [];
-
-    if (typeof window !== 'undefined' && typeof window.location !== 'undefined'){
-        let query = decodeURI(window.location.search);
-        if(query && query.length > 0){
-            if(query.split('?').length > 1){
-                let searchQuery = query.split('?')[1];
-                if(searchQuery.split('&').length > 0){
-                    let paramList = searchQuery.split('&');
-    
-                    if (paramList.length > 0){
-                        paramList.forEach((e) => {
-                            if (e.split('=').length > 1){
-                                let param = {
-                                    key: e.split('=')[0],
-                                    value: e.split('=')[1]
-                                }
-                                params.push(param);
-                            }
-                        });
-                    }
-                }
-            }
-        }
-    }
-
-    return params;
-}
-
-const selectQueryParamValue = (key) => {
-    let params = getQueryParams();
-    let val = '';
-    let filter1 = params.filter(x => x.key === key);
-    if (filter1 && filter1.length > 0){
-        if (filter1.some(x => x.key && x.value)){
-            filter1.forEach(x => {
-                if (x.value){
-                    val = x.value;
-                }
-            });
-        }
-    }
-
-    return val;
-};
-
 const CardXListComponent = (props) => {
-    const { classes } = props; 
-    // const searchParams = getSearchParams();    
-    let filter = selectQueryParamValue('productType1')
-
-    let pageTitle = 'All Products';
-    if (filter && filter.length > 0){
-        pageTitle = filter;
-    }
-    
-    let edges = props.data.allMarkdownRemark.edges.filter(x => x.node.frontmatter.productType1===filter);
+    const { classes } = props;
 
     return (
         <div className={classes.root}>
-            <Heading headerText={pageTitle} />
+            <Heading headerText={props.data.pageTitle} />
             <Grid container spacing={3}>
                 {
-                    edges.map((edge, index)=> {
+                    props.data.edges.map((edge, index)=> {
                         let postLink = "/products/" + edge.node.fields.slug;
                         // const link = '';
                         if (edge.node.frontmatter.productType1===''){
@@ -157,17 +67,9 @@ const CardXListComponent = (props) => {
 }
 
 const CardXList = (props) => {
-    // console.log('CardXList');
-    // console.log(props);
-
+    console.log(props);
     return (
-        <StaticQuery
-            query={query}
-            render={data => (
-                // data = data.allMarkdownRemark.edges.filter(x => x.))
-                <CardXListComponent data={data} {...props}/>
-            )}
-        />
+        <CardXListComponent {...props} />
     )
 }; 
 
--- src/pages/productlist.js
+++ src/pages/productlist.js
@@ -1,11 +1,120 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import ProductsList from '../components/ProductsList';
+import { graphql, StaticQuery } from "gatsby";
 
-export default function Index() {
+
+const query = graphql`
+    query {
+        allMarkdownRemark(sort: {fields: frontmatter___sequence}, filter: {fileAbsolutePath: {regex: "/src/content/products/"}, frontmatter: {featured: {}, productType1: {}, productType2: {}}}) {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  description
+                  date(formatString: "MM/DD/yyyy")
+                  image {
+                    publicURL
+                    childImageSharp {
+                        fluid {
+                          src
+                        }
+                      }
+                  }
+                  link
+                  featured
+                  productType1
+                  productType2
+                }
+                fileAbsolutePath
+                fields {
+                  slug
+                }
+              }
+            }
+          }
+        }  
+`;
+
+const getQueryParams = () => {
+    let params = [];
+
+    if (typeof window !== 'undefined' && typeof window.location !== 'undefined'){
+        let query = decodeURI(window.location.search);
+        if(query && query.length > 0){
+            if(query.split('?').length > 1){
+                let searchQuery = query.split('?')[1];
+                if(searchQuery.split('&').length > 0){
+                    let paramList = searchQuery.split('&');
+    
+                    if (paramList.length > 0){
+                        paramList.forEach((e) => {
+                            if (e.split('=').length > 1){
+                                let param = {
+                                    key: e.split('=')[0],
+                                    value: e.split('=')[1]
+                                }
+                                params.push(param);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    return params;
+}
+
+const selectQueryParamValue = (key) => {
+    let params = getQueryParams();
+    let val = '';
+    let filter1 = params.filter(x => x.key === key);
+    if (filter1 && filter1.length > 0){
+        if (filter1.some(x => x.key && x.value)){
+            filter1.forEach(x => {
+                if (x.value){
+                    val = x.value;
+                }
+            });
+        }
+    }
+
+    return val;
+};
+
+const ProductListPageComponent = (props) => { 
+  // const searchParams = getSearchParams();    
+  let filter = selectQueryParamValue('productType1')
+
+  let pageTitle = 'All Products';
+  if (filter && filter.length > 0){
+      pageTitle = filter;
+  }
+  
+  let edges = props.data.allMarkdownRemark.edges.filter(x => x.node.frontmatter.productType1===filter);
+
+  props.data.pageTitle = pageTitle;
+  props.data.filter = filter;
+  props.data.edges = edges;
+
   return (
     <Layout>
-      <ProductsList />
+      <ProductsList {...props}/>
     </Layout>
   );
 }
+
+
+const ProductListPage = (props) => {
+  return (
+      <StaticQuery
+          query={query}
+          render={data => (
+              <ProductListPageComponent data={data} {...props}/>
+          )}
+      />
+  )
+};
+
+export default ProductListPage;
